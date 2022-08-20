ee581515daa9ce1bc1d6794f97596f46aeb6ad39
tayjanks <taylorjenkins801@gmail.com>
2022-08-20 15:55:59 -0600

    cleaned up statements used if else rather than all if

--- maxOfTwo.js
+++ maxOfTwo.js
@@ -1,8 +1,10 @@
-let x = 1
-let y = 2
+let x = 60
+let y = 5
 
 // Write your code below this line.
 
-if (x>y){console.log(x);};
-if (y>x){console.log(y);};
-if (x === y){console.log("The values are identical");};
+if (x > y){console.log(x);
+};
+else if (y > x){console.log(y);
+};
+else {console.log("The values are identical");};

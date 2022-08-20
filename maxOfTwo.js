8597c243fecea11981b4a534c10a628b4e071dcc
tayjanks <taylorjenkins801@gmail.com>
2022-08-20 16:03:19 -0600

    Removing unnecessary semi colons

--- maxOfTwo.js
+++ maxOfTwo.js
@@ -3,8 +3,9 @@ let y = 5
 
 // Write your code below this line.
 
-if (x > y){console.log(x);
-};
-else if (y > x){console.log(y);
-};
-else {console.log("The values are identical");};
+if (x > y){
+    console.log(x)
+} else if (y > x){
+    console.log(y)
+}else [
+    console.log("The values are identical")];
